3727d30e0f559c50384c1b45e74edbeede42ac67
Mutroach <genolet.lucas@gmail.com>
2021-08-29 15:18:12 -0300

    animacion de entrada y ajustes varios

--- PokeScript.js
+++ PokeScript.js
@@ -1,18 +1,27 @@
-let pokeNumber = Math.round(Math.random() * (150 - 1) + 1 );
-let pokeurl = ("https://pokeapi.co/api/v2/pokemon/"+pokeNumber);
-let pokeSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokeNumber+".png";
+function pokeRandom(){
+    let pokeNumber = Math.round(Math.random() * (150 - 1) + 1 );
+    let pokeurl = ("https://pokeapi.co/api/v2/pokemon/"+pokeNumber);
+    let pokeSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokeNumber+".png";
 
-fetch(pokeurl)
-.then(response => response.json())
-.then(data => {
+    fetch(pokeurl)
+    .then(response => response.json())
+    .then(data => {
+        let habilidad = data.abilities[0].ability.name;
+        let alto = data.height;
+        let peso = data.weight;
+        let nombre = data.name;
 
-    let habilidad = data.abilities[0].ability.name;
-    let alto = data.height;
-    let peso = data.weight;
-    let nombre = data.name;
+        var mayus = nombre.split(" ");
+        for (var i = 0; i < mayus.length; i++) {
+            mayus[i] = mayus[i].charAt(0).toUpperCase() + mayus[i].slice(1);
+        } 
+        var pokeNombreMayuscula = mayus.join(" ");
 
-//    document.getElementById("central").innerHTML = nombre;
-    document.getElementById("imagen").innerHTML = `<img src=`+pokeSprite+` id="img">`;
-    document.getElementById("nombre").innerHTML = nombre;
-})
+        document.getElementById("imagenWhite").innerHTML = `<img class="bynfilter" src=`+pokeSprite+` id="img">`;
+        document.getElementById("imagen").innerHTML = `<img src=`+pokeSprite+` id="img">`;
+        document.getElementById("nombre").innerHTML = pokeNombreMayuscula;
+    })
+}
+pokeRandom();
+
 
