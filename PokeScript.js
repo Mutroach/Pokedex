97c9c58f21add53dcfbb724adb598f5cb67e2ed3
Mutroach <genolet.lucas@gmail.com>
2021-08-29 16:46:53 -0300

    nuevos botones

--- PokeScript.js
+++ PokeScript.js
@@ -1,4 +1,8 @@
-function pokeRandom(){
+let peso = "";
+let altura = "";
+let movimientos = "";
+
+function pokeRandom() {
     let pokeNumber = Math.round(Math.random() * (150 - 1) + 1 );
     let pokeurl = ("https://pokeapi.co/api/v2/pokemon/"+pokeNumber);
     let pokeSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokeNumber+".png";
@@ -6,9 +10,9 @@ function pokeRandom(){
     fetch(pokeurl)
     .then(response => response.json())
     .then(data => {
-        let habilidad = data.abilities[0].ability.name;
-        let alto = data.height;
-        let peso = data.weight;
+        movimientos = data.abilities[0].ability.name;
+        altura = data.height;
+        peso = data.weight;
         let nombre = data.name;
 
         var mayus = nombre.split(" ");
@@ -20,8 +24,19 @@ function pokeRandom(){
         document.getElementById("imagenWhite").innerHTML = `<img class="bynfilter" src=`+pokeSprite+` id="img">`;
         document.getElementById("imagen").innerHTML = `<img src=`+pokeSprite+` id="img">`;
         document.getElementById("nombre").innerHTML = pokeNombreMayuscula;
+        document.getElementById("stats").innerHTML = "";
     })
 }
 pokeRandom();
 
+function pokePeso() {
+    document.getElementById("stats").innerHTML = '<div class="stats" id="peso"><p>Peso: ' + peso + '</p></div>';
+}
 
+function pokeAltura() {
+    document.getElementById("stats").innerHTML = '<div class="stats" id="altura"><p>Altura: ' + altura + '</p></div>';
+}
+
+function pokeMovimientos() {
+    document.getElementById("stats").innerHTML = '<div class="stats" id="movimientos"><p>Movimientos: ' + movimientos + '</p></div>';
+}
